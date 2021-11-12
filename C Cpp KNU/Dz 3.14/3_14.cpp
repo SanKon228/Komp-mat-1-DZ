6fe988c2f1c9c301e728836354a06ff83b941976
San Kon <konyk226@gmail.com>
2021-11-13 00:03:54 +0200

    3_3

---
+++ C Cpp KNU/Dz 3.14/3_14.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	int x, y, b, c, a,r;
+	cout << "x= ";
+	cin >> x;
+	cout << "y= ";
+	cin >> y;
+	cout << "b= ";
+	cin >> b;
+	cout << "c= ";
+	cin >> c;
+	cout << "a= ";
+	cin >> a;
+	r = sqrt(pow(x,2) + pow(y,2));
+	if (b <= y && y <= b + pow(c, 2)) {
+
+	}
+}
